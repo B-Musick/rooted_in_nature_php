d59115b47e66d96748c6020badad7ce8e317104d
B-Musick <bmuze1@gmail.com>
2019-11-28 18:01:57 -0700

    Landing page is styled and completed

--- views/landing.php
+++ views/landing.php
@@ -1,7 +1,14 @@
 <?php include 'partials/header.html' ?>
     <div id="landing-page-container">
-        <svg width= "100vw" height="260px" id="landing-svg">
-                
-        </svg>
+
+        <?php
+            
+            $rectWidth = [10,18,30,60,50,70,90,65,55,30,15,5,2,4,7,20,40,20,10,15];
+            for($x = count($rectWidth)-1; $x>=0; $x--){
+                echo '<div class="rectangle" style="width:'.$rectWidth[$x].'vw;height:15px;"></div>';
+            }
+            
+        ?>
+        <div id="landing-main-page">Where science meets nature...</div>
     </div>
 <?php include 'partials/footer.html' ?>
